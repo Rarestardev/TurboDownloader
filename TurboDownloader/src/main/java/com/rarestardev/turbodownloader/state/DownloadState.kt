bca7f700efaddb3ba1552cce4cdc3cd3a5ec6377
Rarestardev <Rarestar.dev@gmail.com>
2026-07-21 09:34:07 +0330

    update DownloadState.kt for save status and show to ui

--- TurboDownloader/src/main/java/com/rarestardev/turbodownloader/state/DownloadState.kt
+++ TurboDownloader/src/main/java/com/rarestardev/turbodownloader/state/DownloadState.kt
@@ -7,15 +7,36 @@ import java.io.File
 value class DownloadId(val value: String)
 
 sealed class DownloadState {
-    object Idle : DownloadState()
-    data class Queued(val id: DownloadId) : DownloadState()
+    data class Idle(val status: DownloadStatus = DownloadStatus.IDLE) : DownloadState()
+    data class Queued(val id: DownloadId, val status: DownloadStatus = DownloadStatus.QUEUED) :
+        DownloadState()
+
     data class Running(
         val id: DownloadId,
-        val progress: DownloadProgress
+        val progress: DownloadProgress,
+        val status: DownloadStatus = DownloadStatus.RUNNING
     ) : DownloadState()
 
-    data class Paused(val id: DownloadId, val progress: DownloadProgress) : DownloadState()
-    data class Completed(val id: DownloadId, val file: File) : DownloadState()
-    data class Failed(val id: DownloadId, val error: Throwable?) : DownloadState()
-    data class Cancelled(val id: DownloadId) : DownloadState()
+    data class Paused(
+        val id: DownloadId,
+        val progress: DownloadProgress,
+        val status: DownloadStatus = DownloadStatus.PAUSED
+    ) : DownloadState()
+
+    data class Completed(
+        val id: DownloadId,
+        val file: File,
+        val status: DownloadStatus = DownloadStatus.COMPLETED
+    ) : DownloadState()
+
+    data class Failed(
+        val id: DownloadId,
+        val error: Throwable?,
+        val status: DownloadStatus = DownloadStatus.FAILED
+    ) : DownloadState()
+
+    data class Cancelled(
+        val id: DownloadId,
+        val status: DownloadStatus = DownloadStatus.CANCELLED
+    ) : DownloadState()
 }
