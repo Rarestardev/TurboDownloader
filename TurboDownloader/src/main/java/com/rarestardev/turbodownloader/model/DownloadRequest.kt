f378b2f6dcadc74c0c5a4cef5278dc2439630a22
Rarestardev <Rarestar.dev@gmail.com>
2026-07-22 12:31:48 +0330

    added autoThreading value in Download request

--- TurboDownloader/src/main/java/com/rarestardev/turbodownloader/model/DownloadRequest.kt
+++ TurboDownloader/src/main/java/com/rarestardev/turbodownloader/model/DownloadRequest.kt
@@ -3,5 +3,6 @@ package com.rarestardev.turbodownloader.model
 data class DownloadRequest(
     val uri: String,
     val fileName: String,
-    val threadCount: Int = 4
+    val threadCount: Int = 4,
+    val autoThreading: Boolean = false
 )
