6decbd2735d79b83f48b8f1ed5e9eb00c02e93e0
Rarestardev <Rarestar.dev@gmail.com>
2026-08-18 18:01:19 +0330

    update database version

--- TurboDownloader/src/main/java/com/rarestardev/turbodownloader/storage/DownloadDatabase.kt
+++ TurboDownloader/src/main/java/com/rarestardev/turbodownloader/storage/DownloadDatabase.kt
@@ -3,7 +3,7 @@ package com.rarestardev.turbodownloader.storage
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [DownloadEntity::class, ChunkEntity::class], version = 1)
+@Database(entities = [DownloadEntity::class, ChunkEntity::class], version = 2)
 abstract class DownloadDatabase : RoomDatabase(){
     abstract fun dao() : DownloadDao
 }
